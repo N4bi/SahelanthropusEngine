7e5125ebe25832445feb32226908e29c5d149adb
Iván Pérez Latorre <latorrem10@gmail.com>
2016-09-13 23:36:05 +0200

    tests

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -39,9 +39,24 @@ bool ModuleSceneIntro::CleanUp()
 // Update: draw background
 update_status ModuleSceneIntro::Update(float dt)
 {
+	ImGuiWindowFlags window_flags = 0;
 	ImGui::ShowTestWindow();
-	//ImGui::BeginMenu("Menu");
-	//ImGui::Text("Hello");
+	if (ImGui::BeginMenu("Menu"))
+	{
+		ImGui::Text("Menu Clicked!");
+		ImGui::EndMenu();
+	}
+	ImGui::BulletText("This is a bullet text");
+	ImGui::Button("Button", ImVec2(50, 30));
+	if (ImGui::BeginPopup("popup"))
+	{
+		ImGui::Text("PopUp clicked!");
+		ImGui::EndPopup();
+	}
+	ImGui::Text("Hello");
+	ImGui::LabelText("LABEL", "text");
+
+
 	Plane(0, 1, 0, 0).Render();
 	
 
